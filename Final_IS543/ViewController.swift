6234463073bb85360ece75bdbe0d71de4d11a97d
Kyle Pontius <kbpontius@gmail.com>
2015-12-17 11:05:10 -0700

    Initial Commit

---
+++ Final_IS543/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final_IS543
+//
+//  Created by Kyle on 12/17/15.
+//  Copyright © 2015 Kyle Pontius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
